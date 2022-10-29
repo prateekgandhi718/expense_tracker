416aa759278696b1e898f9c56201f62f4ab859fb
Prateek <prateekgandhi718@gmail.com>
2022-10-29 19:15:00 +0530

    all apis

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from core.views import ExpenseAPIView
+from core.views import ApprovalRejectionAPIView, ExpenseAPIView, ExpenseDetailAPIView
 
 
 urlpatterns = [
-    path('expense', ExpenseAPIView.as_view(), name = 'expense')
+    path('expense', ExpenseAPIView.as_view(), name = 'expense'),
+    path('expense-detail', ExpenseDetailAPIView.as_view(), name = 'expense-detail'),
+    path('expense-approval', ApprovalRejectionAPIView.as_view(), name = 'approval-rejection'),
 ]
