016302ec5ab68ecda7bdb1a102893456314d575c
Prateek <prateekgandhi718@gmail.com>
2022-10-30 14:03:13 +0530

    cors enable

--- expense/settings.py
+++ expense/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-=y&tgock9j(hd977o=c!_9hd77mrxy+vjs*(%b7#)4pe$wg1bp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 
 # Application definition
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
 
     'core',
     'rest_framework',
@@ -51,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'expense.urls'
